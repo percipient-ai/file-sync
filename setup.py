a57b9d49c3221745e6e055d266db4eda98d52614
Jerome Berclaz <jerome@percipient.ai>
2020-06-15 12:07:48 -0700

    Restructured the repo

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(
     author="percipient.ai",
     license="Proprietary",
     include_package_data=True,
+    packages=find_packages(),
     package_data={
         "file_sync": [
             "config/logging_config.ini",
         ]
     },
-    entry_points={"console_scripts": ["file_sync=agent:main"]},
+    entry_points={"console_scripts": ["file_sync=file_sync.agent:main"]},
 )
